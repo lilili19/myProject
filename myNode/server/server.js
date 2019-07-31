6fa8fd05176518b51c7efff4d06ebb4568dd6391
lilili19 <769184393@qq.com>
2019-07-31 10:39:54 +0800

    1

--- myNode/server/server.js
+++ myNode/server/server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000);
 
 //添加路由到应用上
-app.use('/', require('../routes/fileDown'));
+// app.use('/', require('../routes/fileDown'));
 app.use('/user', require('../routes/user'));
 app.use('/menu', require('../routes/menu'));
 app.use('/project', require('../routes/project'));
--- myVue/config/index.js
+++ myVue/config/index.js
@@ -65,11 +65,11 @@ module.exports = {
 
     build: {
       // Template for index.html
-      index: path.resolve(__dirname, '../dist/overview.html'),
+      index: path.resolve(__dirname, '../dist/index.html'),
 
       // Paths
       assetsRoot: path.resolve(__dirname, '../dist'),
-      assetsSubDirectory: './overview',
+      assetsSubDirectory: './static',
       assetsPublicPath: '/',
 
       /**
