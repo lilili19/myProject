31d90b9e700a5110113d6fd458bbdfd89d4bcc2f
lilili19 <769184393@qq.com>
2019-08-01 11:33:23 +0800

    1

--- myNode/config.js
+++ myNode/config.js
@@ -6,5 +6,5 @@ module.exports = {
     sqlName: 'user', // 数据库表1
     sqlName2: 'business', // 数据库表2
     host: path === 'dev' ? '/api' : '', // 接口
-    mySqlPassword: '123456' // mySql 密码
+    mySqlPassword: path === 'dev' ? '123456': 'chero@123' // mySql 密码
 }
